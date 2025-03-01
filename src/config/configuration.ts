9e065b313798869ba36401cb8abc1ae97ac5ee4c
Ashutosh <ashutosh.singh@amoghbuildtech.com>
2025-03-01 12:14:21 +0530

    connection

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,8 +3,7 @@ export default () => ({
   database: {
     uri:
       process.env.DB_URI ||
-      'mongodb://localhost:27017/hramoghbuildtech?authSource=amoghbuildtech'
-       // 'mongodb://amoghbuildtechDBA:kjX3zimswjP40KON14Bf@10.10.2.97:27017/amoghbuildtech?authSource=amoghbuildtech'//'mongodb://127.0.0.1:27017/amoghbuildtech'
+      'mongodb://DBA:Sh3TbYhx8ypJPxmtfL@127.0.0.1:27017/hramoghbuildtech?authSource=admin'
     },
   auth: {
     tokenExpiry: process.env.TOKEN_EXPIRY || '18h',
