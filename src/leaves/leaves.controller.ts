d55919c28d03d0a67c3dfb10ed073fdc8fe9240d
Ashutosh <ashutosh.singh@amoghbuildtech.com>
2025-07-12 15:16:37 +0530

    leaves

--- src/leaves/leaves.controller.ts
+++ src/leaves/leaves.controller.ts
@@ -10,126 +10,142 @@ import { AuthGuard } from 'src/auth/auth.guard';
 @UseGuards(AuthGuard)
 @Controller('/leaves')
 export class LeavesController {
-    constructor(
-        private leaveService:LeavesService,
-        private contextService:RequestContextService,
-        private cacheService:CacheService,
+  constructor(
+    private leaveService: LeavesService,
+    private contextService: RequestContextService,
+    private cacheService: CacheService,
 
-    ){  }
-    @Post('/create-application')
-    async create(@Body() payload:any){
-       let response=await this.leaveService.create(payload)
-       return response
-    }
-    // @Get('/:id')
-    // async notification(@Param('id') id:any){
-    //   id= Types.ObjectId.createFromHexString(id)
-    //   let response=  await this.leaveService.findOne({userId:id})
-    //     return response 
+  ) { }
+  @Post('/create-application')
+  async create(@Body() payload: any) {
+    let response = await this.leaveService.create(payload)
+    return response
+  }
+  // @Get('/:id')
+  // async notification(@Param('id') id:any){
+  //   id= Types.ObjectId.createFromHexString(id)
+  //   let response=  await this.leaveService.findOne({userId:id})
+  //     return response 
 
-    // }
-    @Get("/application")
-    async getNotification(@Param() params:any){
-      console.log("pp",params)
-      const pageNumber: number = Number(params?.pageNumber) || 0;
+  // }
+  @Get("/application")
+  async getNotification(@Query() params: any) {
+    console.log("pp", params)
+    const pageNumber: number = Number(params?.pageNumber) || 0;
     const limit: number = Number(params?.size) || 8;
-    const skip: number = pageNumber*limit;
+    const skip: number = pageNumber * limit;
     const sortKey: string = params?.sortKey || 'createdAt';
     const sortDir: string = params?.sortDir || 'DESC';
-      let query={}
-       
-      let userId: string = this.contextService.get('userId');
-      let department: string = this.contextService.get('department');
-      let role: string = this.contextService.get('role');
+    let query = {}
+
+    let userId: string = this.contextService.get('userId');
+    let department: string = this.contextService.get('department');
+    let role: string = this.contextService.get('role');
 
 
     // if(params?.userId)  query['userId'] = new Types.ObjectId(userId); 
-     switch(role){
-            case Roles.ADMIN:
-           console.log("ADMIN")
-            break;
-            case Roles.MANAGER:
-            case Roles.TEAM_LEAD:
-    
-              if( department?.includes(Department.HR)){
-               
-              }
-              else{
-                let team=await this.cacheService.getTeamByManager(userId)
-                console.log("tes",team)
-                let employeeCodes=[] 
-                let serialNumbers=[]
-               await Promise.all(team.map(async(user:any)=>{
-                  let userData=await this.cacheService.getUserData(user)
-                  console.log("user",userData)
-                  employeeCodes.push(userData.employeeCode)
-                  serialNumbers.push(userData.machineNumber)
-                }))
-                console.log("uuu",employeeCodes,serialNumbers) 
-              
-              }
-              break;
-            
-            case Roles.MARKETING_MANAGER:
-            
-            case Roles.AGENT:
-              if(department?.includes(Department.HR)){
-                console.log("AGET finance case 1")
-              }
-              else{
+    switch (role) {
+      case Roles.ADMIN:
+        console.log("ADMIN")
+        break;
+      case Roles.MANAGER:
+      case Roles.TEAM_LEAD:
 
-                  query['userId']= new Types.ObjectId(userId)
+        if (department?.includes(Department.HR)) {
 
-                }
-              break;
-            default:
-              console.log("default case")
-    
-              query['userId']=userId
-            break
-          }
-      let response=  await this.leaveService.findLeaveApplication(skip,
-        limit,
-        sortKey,
-        sortDir,query)
-        return response 
+        }
+        else {
+          let team = await this.cacheService.getTeamByManager(userId)
+          console.log("tes", team)
+          let employeeCodes = []
+          let serialNumbers = []
+          await Promise.all(team.map(async (user: any) => {
+            let userData = await this.cacheService.getUserData(user)
+            console.log("user", userData)
+            employeeCodes.push(userData.employeeCode)
+            serialNumbers.push(userData.machineNumber)
+          }))
+          console.log("uuu", employeeCodes, serialNumbers)
 
-    }
-    @Patch('/:id')
-    async updateNotification(@Body() params:any, @Param('id') id: string){
-     let response=await this.leaveService.update({_id:id},params)
-      return response
-    }
-    @Delete('/:id')
-    async lead(@Param('id') id: string){
-     let response=await this.leaveService.delete({_id:id})
-      return response
-    }
-    @Post('/create-leave')
-    async createLeave(@Body() payload:any){
-       let response=await this.leaveService.createLeave(payload)
-       return response
-    }
-    @Get('/:id')
-    async getOneLeave(@Param('id') id:any){
-      id= Types.ObjectId.createFromHexString(id)
-      let response=  await this.leaveService.findOne({userId:id})
-        return response 
+        }
+        break;
 
+      case Roles.MARKETING_MANAGER:
+
+      case Roles.AGENT:
+        if (department?.includes(Department.HR)) {
+          console.log("AGET finance case 1")
+        }
+        else {
+
+          query['userId'] = new Types.ObjectId(userId)
+
+        }
+        break;
+      default:
+        console.log("default case")
+
+        query['userId'] = userId
+        break
     }
-    @Get()
-    async getLeave(@Query() params:any){
-      const pageNumber: number = Number(params?.pageNumber) || 0;
+    if (params?.startTime) {
+      const startTime = params.startTime;
+      let endTime = new Date().toISOString();
+      if (params?.endTime) {
+        endTime = params.endTime;
+      }
+
+      query['$or'] = [
+        { fromDate: { $gte: new Date(startTime), $lt: new Date(endTime) } },
+        { toDate: { $gte: new Date(startTime), $lt: new Date(endTime) } }
+      ];
+    }
+    if (params?.userId) {
+      query['userId'] = Types.ObjectId.createFromHexString(params.userId);
+    }
+
+    let response = await this.leaveService.findLeaveApplication(skip,
+      limit,
+      sortKey,
+      sortDir, query)
+    return response
+
+  }
+  @Patch('/:id')
+  async updateNotification(@Body() params: any, @Param('id') id: string) {
+    let response = await this.leaveService.update({ _id: id }, params)
+    return response
+  }
+  @Delete('/:id')
+  async lead(@Param('id') id: string) {
+    let response = await this.leaveService.delete({ _id: id })
+    return response
+  }
+  @Post('/create-leave')
+  async createLeave(@Body() payload: any) {
+    let response = await this.leaveService.createLeave(payload)
+    return response
+  }
+  @Get('/:id')
+  async getOneLeave(@Param('id') id: any) {
+    id = Types.ObjectId.createFromHexString(id)
+    let response = await this.leaveService.findOne({ userId: id })
+    return response
+
+  }
+  @Get()
+  async getLeave(@Query() params: any) {
+    const pageNumber: number = Number(params?.pageNumber) || 0;
     const limit: number = Number(params?.size) || 8;
-    const skip: number = pageNumber*limit;
+    const skip: number = pageNumber * limit;
     const sortKey: string = params?.sortKey || 'createdAt';
     const sortDir: string = params?.sortDir || 'DESC';
-      let query={}
-       
-      let userId: string = this.contextService.get('userId');
-      let role: string = this.contextService.get('role');
+    let query = {}
 
-      if (params?.startTime) {
+    let userId: string = this.contextService.get('userId');
+    let role: string = this.contextService.get('role');
+
+    if (params?.startTime) {
       const startTime = params.startTime;
       let endTime = new Date().toISOString();
       if (params?.endTime) {
@@ -142,43 +158,43 @@ export class LeavesController {
       query['createdAt'] = value;
     }
 
-    console.log("role",role)
-        console.log("useId",userId)
-      query['userId'] = new Types.ObjectId(userId);
-       switch (role) {            
-            case Roles.MANAGER:
-              case Roles.TEAM_LEAD:
-              {
-                let userIds: string[] = await this.cacheService.getTeamByManager(userId);
-                const objectIds = userIds.map(id => new Types.ObjectId(id));
-                objectIds.push(new Types.ObjectId(userId));
-                userIds.push(userId);
-                query['userId']={$in:objectIds }
-              }
-              break;
-            case Roles.AGENT: {
-             query['userId']=new Types.ObjectId(userId)
-      
-            }
-            default:{
-      // query['userId']=new Types.ObjectId(userId)
+    console.log("role", role)
+    console.log("useId", userId)
+    query['userId'] = new Types.ObjectId(userId);
+    switch (role) {
+      case Roles.MANAGER:
+      case Roles.TEAM_LEAD:
+        {
+          let userIds: string[] = await this.cacheService.getTeamByManager(userId);
+          const objectIds = userIds.map(id => new Types.ObjectId(id));
+          objectIds.push(new Types.ObjectId(userId));
+          userIds.push(userId);
+          query['userId'] = { $in: objectIds }
+        }
+        break;
+      case Roles.AGENT: {
+        query['userId'] = new Types.ObjectId(userId)
 
-            }
-          
-          }
-      if(params?.userId){
-        query['userId'] = new Types.ObjectId(params.userId.toString());
       }
-      let response=  await this.leaveService.findLeave(skip,
-        limit,
-        sortKey,
-        sortDir,query)
-        return response 
+      default: {
+        // query['userId']=new Types.ObjectId(userId)
+
+      }
 
     }
-    @Patch('add/:id')
-    async addInArray(@Body() params:any, @Param('id') id: string){
-     let response=await this.leaveService.addInArray(id,params?.field,params.value,params?.userId)
-      return response
+    if (params?.userId) {
+      query['userId'] = new Types.ObjectId(params.userId.toString());
     }
+    let response = await this.leaveService.findLeave(skip,
+      limit,
+      sortKey,
+      sortDir, query)
+    return response
+
+  }
+  @Patch('add/:id')
+  async addInArray(@Body() params: any, @Param('id') id: string) {
+    let response = await this.leaveService.addInArray(id, params?.field, params.value, params?.userId)
+    return response
+  }
 }
--- src/on-duty/on-duty.controller.ts
+++ src/on-duty/on-duty.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { privateDecrypt } from 'crypto';
 import { RequestContextService } from 'src/shared/request-context/request-context.service';
 import { Types } from 'mongoose';
@@ -10,129 +10,132 @@ import { AuthGuard } from 'src/auth/auth.guard';
 @UseGuards(AuthGuard)
 @Controller('/onDuty')
 export class OnDutyController {
-    constructor(
-        private onDutyService:OnDutyService,
-        private contextService:RequestContextService,
-        private cacheService:CacheService
+  constructor(
+    private onDutyService: OnDutyService,
+    private contextService: RequestContextService,
+    private cacheService: CacheService
 
-    ){  }
-    @Post()
-    async create(@Body() payload:any){
-       let response=await this.onDutyService.create(payload)
-       return response
-    }
-    @Get('/:id')
-    async notification(@Param('id') id:any){
-      id= Types.ObjectId.createFromHexString(id)
-      let response=  await this.onDutyService.findOne({userId:id})
-        return response 
+  ) { }
+  @Post()
+  async create(@Body() payload: any) {
+    let response = await this.onDutyService.create(payload)
+    return response
+  }
+  @Get('/:id')
+  async notification(@Param('id') id: any) {
+    id = Types.ObjectId.createFromHexString(id)
+    let response = await this.onDutyService.findOne({ userId: id })
+    return response
 
-    }
-    @Get()
-    async getNotification(@Param() params:any){
-      console.log("pp",params)
-      const pageNumber: number = Number(params?.pageNumber) || 0;
+  }
+  @Get()
+  async getNotification(@Query() params: any) {
+    console.log("pp", params)
+    const pageNumber: number = Number(params?.pageNumber) || 0;
     const limit: number = Number(params?.size) || 8;
-    const skip: number = pageNumber*limit;
+    const skip: number = pageNumber * limit;
     const sortKey: string = params?.sortKey || 'createdAt';
     const sortDir: string = params?.sortDir || 'DESC';
-      let query={}
-       
-      let userId: string = this.contextService.get('userId');
-       let department: string = this.contextService.get('department');
+    let query = {}
 
-      let role: string = this.contextService.get('role');
-      console.log("role",role)
-      console.log("userId",userId)
+    let userId: string = this.contextService.get('userId');
+    let department: string = this.contextService.get('department');
+
+    let role: string = this.contextService.get('role');
+    console.log("role", role)
+    console.log("userId", userId)
     if (params?.startTime) {
       const startTime = params.startTime;
       let endTime = new Date().toISOString();
       if (params?.endTime) {
         endTime = params.endTime;
       }
-      const value: any = {
-        $gte: new Date(startTime),
-        $lt: new Date(endTime),
-      };
-      query['createdAt'] = value;
+
+      query['$or'] = [
+        { fromDate: { $gte: new Date(startTime), $lt: new Date(endTime) } },
+        { toDate: { $gte: new Date(startTime), $lt: new Date(endTime) } }
+      ];
+    }
+    if (params?.userId) {
+      query['userId'] = Types.ObjectId.createFromHexString(params.userId);
     }
 
-        switch (role) {    
-                  
-            case Roles.MANAGER:
-              case Roles.TEAM_LEAD:
-              {
-                if(department==Department.HR){
+    switch (role) {
 
-              }
-              else{
-                let userIds: string[] = await this.cacheService.getTeamByManager(userId);
-                const objectIds = userIds.map(id => new Types.ObjectId(id));
-                objectIds.push(new Types.ObjectId(userId));
-                userIds.push(userId);
-                query['userId']={$in:objectIds }
-              }
-              }
-              break;
-            case Roles.AGENT:{
-              if(department==Department.HR){
+      case Roles.MANAGER:
+      case Roles.TEAM_LEAD:
+        {
+          if (department == Department.HR) {
 
-              }
-             query['userId']=new Types.ObjectId(userId)
-      
-            }
-            default:{
-      // query['userId']=params.userId
-
-            }
-           
           }
-      let response=  await this.onDutyService.findLeaveApplication(skip,
-        limit,
-        sortKey,
-        sortDir,query)
-        return response 
+          else {
+            let userIds: string[] = await this.cacheService.getTeamByManager(userId);
+            const objectIds = userIds.map(id => new Types.ObjectId(id));
+            objectIds.push(new Types.ObjectId(userId));
+            userIds.push(userId);
+            query['userId'] = { $in: objectIds }
+          }
+        }
+        break;
+      case Roles.AGENT: {
+        if (department == Department.HR) {
+
+        }
+        query['userId'] = new Types.ObjectId(userId)
+
+      }
+      default: {
+        // query['userId']=params.userId
+
+      }
 
     }
-    @Patch('/:id')
-    async updateNotification(@Body() params:any, @Param('id') id: string){
-     let response=await this.onDutyService.updatePermission({_id:id},params)
-      return response
-    }
-    @Delete('/:id')
-    async lead(@Param('id') id: string){
-     let response=await this.onDutyService.delete({_id:id})
-      return response
-    }
+    let response = await this.onDutyService.findLeaveApplication(skip,
+      limit,
+      sortKey,
+      sortDir, query)
+    return response
 
-    @Get('/:id')
-    async getOneLeave(@Param('id') id:any){
-      id= Types.ObjectId.createFromHexString(id)
-      let response=  await this.onDutyService.findOne({userId:id})
-        return response 
+  }
+  @Patch('/:id')
+  async updateNotification(@Body() params: any, @Param('id') id: string) {
+    let response = await this.onDutyService.updatePermission({ _id: id }, params)
+    return response
+  }
+  @Delete('/:id')
+  async lead(@Param('id') id: string) {
+    let response = await this.onDutyService.delete({ _id: id })
+    return response
+  }
 
-    }
-    @Get('')
-    async getLeave(@Body() params:any){
-      const pageNumber: number = Number(params?.pageNumber) || 0;
+  @Get('/:id')
+  async getOneLeave(@Param('id') id: any) {
+    id = Types.ObjectId.createFromHexString(id)
+    let response = await this.onDutyService.findOne({ userId: id })
+    return response
+
+  }
+  @Get('')
+  async getLeave(@Body() params: any) {
+    const pageNumber: number = Number(params?.pageNumber) || 0;
     const limit: number = Number(params?.size) || 8;
-    const skip: number = pageNumber*limit;
+    const skip: number = pageNumber * limit;
     const sortKey: string = params?.sortKey || 'createdAt';
     const sortDir: string = params?.sortDir || 'DESC';
-      let query={}
-       
-      let userId: string = this.contextService.get('userId');
-      // if(params?.isread) query['read']=params.isread
-      let response=  await this.onDutyService.findLeaveApplication(skip,
-        limit,
-        sortKey,
-        sortDir,query)
-        return response 
+    let query = {}
 
-    }
-     @Patch('add/:id')
-    async addInArray(@Body() params:any, @Param('id') id: string){
-     let response=await this.onDutyService.addInArray(id,params?.field,params.value,params?.userId)
-      return response
-    }
+    let userId: string = this.contextService.get('userId');
+    // if(params?.isread) query['read']=params.isread
+    let response = await this.onDutyService.findLeaveApplication(skip,
+      limit,
+      sortKey,
+      sortDir, query)
+    return response
+
+  }
+  @Patch('add/:id')
+  async addInArray(@Body() params: any, @Param('id') id: string) {
+    let response = await this.onDutyService.addInArray(id, params?.field, params.value, params?.userId)
+    return response
+  }
 }
