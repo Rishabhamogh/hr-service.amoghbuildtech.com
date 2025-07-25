d93c1de7cf2a1b7e688bfd8e1a10fcdf85b01773
Ashutosh <ashutosh.singh@amoghbuildtech.com>
2025-07-25 11:45:58 +0530

    unique

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -28,7 +28,7 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ required: false })
+  @Prop({ required: false , unique: true})
   employeeCode: string;
 
   @Prop({ required: false })
